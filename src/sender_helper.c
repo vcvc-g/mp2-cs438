0eae9b38c81572d7b26162b840e658f662323123
Jiayi Wu <jiayiwu@Jiayis-MacBook-Pro.local>
2019-10-05 02:42:32 -0500

    some change in qichao's branch

--- src/sender_helper.c
+++ src/sender_helper.c
@@ -7,16 +7,23 @@
 #define CONGESTION_AVOID 1
 #define FAST_RECOVERY 2
 
+sender_info* senderInfo;
+
 /* 
 Function timeout_interval(): calculate timeout interval for sending packet;
 float estimated_rtt: rtt from last timeout_interval call;
 float sampled_rtt: measured rtt from time();
 */
-float timeout_interval(float estimated_rtt, float sampled_rtt) {
-    // using hard code safty margin for now
-    // dev_rtt = (1-BETA)*dev_rtt + BETA * abs(sampled_rtt-estimated_rtt);
-    return (1-ALPHA)*estimated_rtt + ALPHA*sampled_rtt + 4*SAFETY_MARGIN;
+float timeout_interval(float sampled_rtt) {
+    float estimated_rtt = senderInfo->estimated_rtt;
+    float dev_rtt = senderInfo->dev_rtt;
+    dev_rtt = (1 - BETA) * (dev_rtt) + BETA * abs(sampled_rtt - estimated_rtt);
+    estimated_rtt = (1 - ALPHA) * estimated_rtt + ALPHA * sampled_rtt + 4 * dev_rtt;
+    senderInfo->estimated_rtt = estimated_rtt;
+    senderInfo->dev_rtt = dev_rtt;
+    return estimated_rtt;
 }
+
 
 void adjust_window(struct sender_info *sender){
     int cur_state = sender->congestion_state; 
--- src/sender_helper.h
+++ src/sender_helper.h
@@ -6,6 +6,8 @@
 
 typedef struct sender_info {
     float timeout;
+    float estimated_rtt;
+    float dev_rtt;
     int congestion_state;
     int window_base;
     int window_size;
@@ -14,6 +16,13 @@ typedef struct sender_info {
     struct timeval timer_start;
 
 } sender_info;
+
+
+enum congestion_state {
+    SLOW_START,
+    CONGESTION_AVOID,
+    FAST_RECOVERY,  
+};
 
 
 
