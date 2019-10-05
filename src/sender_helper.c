4fe69ffe9ea3a95899eac9b93cd09c4478e957d0
vcvc-g <qgao10@illinois.edu>
2019-10-05 01:49:30 -0500

    naive code

--- src/sender_helper.c
+++ src/sender_helper.c
@@ -1,1 +1,35 @@
-int read_file(char* filename, )
+#include <sender_helper.h>
+
+#define ALPHA 0.125
+#define BETA  0.25
+#define SAFETY_MARGIN 15
+#define SLOW_START 0
+#define CONGESTION_AVOID 1
+#define FAST_RECOVERY 2
+
+/* 
+Function timeout_interval(): calculate timeout interval for sending packet;
+float estimated_rtt: rtt from last timeout_interval call;
+float sampled_rtt: measured rtt from time();
+*/
+float timeout_interval(float estimated_rtt, float sampled_rtt) {
+    // using hard code safty margin for now
+    // dev_rtt = (1-BETA)*dev_rtt + BETA * abs(sampled_rtt-estimated_rtt);
+    return (1-ALPHA)*estimated_rtt + ALPHA*sampled_rtt + 4*SAFETY_MARGIN;
+}
+
+void adjust_window(struct sender_info *sender){
+    int cur_state = sender->congestion_state; 
+    if (cur_state == SLOW_START) {
+        sender->window_size+=1;
+    }
+
+    else if (cur_state == CONGESTION_AVOID) {
+        sender->window_size+=1;
+    }
+
+    else if (cur_state == FAST_RECOVERY) {
+        sender->window_size*=2;
+    }
+
+}
--- src/sender_helper.h
+++ src/sender_helper.h
@@ -1,10 +1,19 @@
-#define msg_body_size 1460
+#include <stdio.h>
+#include <sys/time.h>
 
-typedef struct data_array{
-    size_t length;
-    char* data;
-    
-}
+#define msg_body_size 1460
+#define SWS 360
+
+typedef struct sender_info {
+    float timeout;
+    int congestion_state;
+    int window_base;
+    int window_size;
+    int ssthresh;
+    int packet[SWS];
+    struct timeval timer_start;
+
+} sender_info;
 
 
 
